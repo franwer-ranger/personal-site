7eb1aa829a0935c025ef71131c035f50af7f6cbb
Francisco Barahona Guzmán <franbaraguz98@gmail.com>
2026-07-30 12:51:13 +0200

    publish page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://franbarahona.dev',
   i18n: {
     locales: ['es', 'en'],
     defaultLocale: 'es',
